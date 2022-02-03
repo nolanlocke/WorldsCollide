1f83f62afc57436a5dfb047cd5f28b2ae2df144a
Kiel <95580337+kielbasiago@users.noreply.github.com>
2022-02-03 18:52:59 -0500

    update bytes sent

--- metadata/objective_metadata_writer.py
+++ metadata/objective_metadata_writer.py
@@ -39,17 +39,34 @@ class ObjectiveMetadataWriter:
         # else:
 
         objective_data = []
-        name_space = self.allocate_name(objective.result.NAME)
-        value = getattr(objective.result, 'count', 255)
+        # write down the name found in the menu
+        name_space = self.allocate_name(objective.result.__str__())
+        # write down the result NAME (this is more or less an ID)
+        type_name_space = self.allocate_name(objective.result.NAME)
+
 
         [name_ptr_high_byte, name_ptr_low_byte]= self.get_high_low_bytes(name_space.start_address - NAME_BANK_INT)
+        [type_name_ptr_high_byte, type_name_ptr_low_byte]= self.get_high_low_bytes(type_name_space.start_address - NAME_BANK_INT)
+
+        count = getattr(objective.result, 'count', None)
+        levels = getattr(objective.result, 'levels', None)
+
         new_data = flatten([
+            # $00
             objective.id,
+            # $01-$02
             name_ptr_high_byte, # high byte of pointer
             name_ptr_low_byte, # low byte of pointer
+            # $03
             len(objective.conditions), # number of conditions affecting objective
+            # $04
             objective.conditions_required, # number of conditions required
-            [255 for f in range(0, 27)]
+            # $05-$06 the result value for the objective 0xFFFF is null
+            self.get_high_low_bytes(count or levels or 0xffff),
+            # $07-$08
+            type_name_ptr_high_byte,
+            type_name_ptr_low_byte,
+            [255 for f in range(0, 23)]
         ])
 
         objective_data += new_data
@@ -65,23 +82,34 @@ class ObjectiveMetadataWriter:
         this_condition_data = []
         condition_type_value = condition.condition_type.value
 
-        name_space = self.allocate_name(condition.NAME)
+        name_space = self.allocate_name(condition.__str__())
         [name_ptr_high_byte, name_ptr_low_byte]= self.get_high_low_bytes(name_space.start_address - NAME_BANK_INT)
+        type_name_space = self.allocate_name(condition.NAME)
+        [type_name_ptr_high_byte, type_name_ptr_low_byte]= self.get_high_low_bytes(type_name_space.start_address - NAME_BANK_INT)
+
 
         this_condition_data += [
+            # $00 arbitrary id
             index,
+            # $01-$02 pointer to name (offset 0xFE0000)
             name_ptr_high_byte,
             name_ptr_low_byte,
+            # $03 condition type enum value - used for data manipulation when reading bits
             condition_type_value if condition.condition_type == ObjectiveConditionType.BattleBit
             else condition_type_value if condition.condition_type == ObjectiveConditionType.Character
             else condition_type_value if condition.condition_type == ObjectiveConditionType.Esper
             else condition_type_value if condition.condition_type == ObjectiveConditionType.EventBit
             else condition_type_value if condition.condition_type == ObjectiveConditionType.EventWord
             else 255,
+            # $04-$05 the base address in memory
             self.get_high_low_bytes(condition.base_address()),
+            # $06-$07 the bit offset in memory
             self.get_high_low_bytes(condition.bit()),
+            # $07-$08 the context value. Used with condition type
             self.get_high_low_bytes(getattr(condition, 'value', 0xffff)),
-            [255 for f in range(0, 6)]
+            type_name_ptr_high_byte,
+            type_name_ptr_low_byte,
+            [255 for f in range(0, 4)]
         ]
 
         this_condition_data = flatten(this_condition_data)
@@ -117,19 +145,21 @@ class ObjectiveMetadataWriter:
             objective_data += flatten(data)
 
         header = flatten([
-            10, # id
-            int(OBJECTIVE_BANK, 16), # bank of objective data
-            2, # high byte - offset for start of objective data (from start of bank)
-            3, # low  byte - offset for start of objective data (from start of bank)
-            4, # high byte - size of objective data
-            5, # low  byte - size of objective data
-            len(self.objectives),
+            10, # $00 arbitrary id
+            int(OBJECTIVE_BANK, 16), # $01 bank of objective data
+            None, # $02 high byte - offset for start of objective data (from start of bank)
+            None, # $03 low  byte - offset for start of objective data (from start of bank)
+            None, # $04 high byte - size of objective data
+            None, # $05 low  byte - size of objective data
+            len(self.objectives), # $06
             [255 for f in range(0, 9)]
         ])
 
         header_space = Allocate(Bank[HEADER_BANK], len(header), "Objective Header")
         objective_space = Allocate(Bank[OBJECTIVE_BANK], len(objective_data), f"Objective Metadata", 0xFF)
+        # set $02-$03
         header[2:4] = self.get_high_low_bytes(objective_space.start_address - OBJECTIVE_BANK_INT)
+        # set $04-$05
         header[4:6] = self.get_high_low_bytes(len(objective_data))
         header_space.write(header)
         objective_space.write(objective_data)
